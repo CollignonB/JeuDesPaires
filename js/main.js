1cd46a19a5e8b6d0c4554ad0f39cdf0a6d852e45
Baptiste COLLIGNON <collignon@CDI>
2020-08-31 17:08:01 +0200

    create cards in html and start creation with JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let divRow = document.getElementById("divRow");
+
+for (let i = 0; i < 12; i++){
+    let cardC = document.createElement("div");   
+    cardC.classList.add("col-3","col-sm-4","col-lg-2");
+    let card = document.createElement("div");
+    card.classList.add("playingCard");
+
+    cardC.appendChild(card);
+    divRow.appendChild(cardC);
+}
+

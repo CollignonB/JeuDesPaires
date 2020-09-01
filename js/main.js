e5c95e69dafe956dcde105083692821761e41aaa
Baptiste COLLIGNON <collignon@CDI>
2020-09-01 12:13:56 +0200

    add protect against double click on same card, and timer between to clicks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let divRow = document.getElementById("divRow");
+let main = document.getElementsByTagName("main")[0];
 let nbCards = 12;
 let compareTable = [];
 let colorTable = [
@@ -15,23 +16,8 @@ let colorTable = [
     "colorFive",
     "colorSix"
 ];
-
-function shuffle(arra1) {
-    var ctr = arra1.length, temp, index;
-
-// While there are elements in the array
-    while (ctr > 0) {
-// Pick a random index
-        index = Math.floor(Math.random() * ctr);
-// Decrease ctr by 1
-        ctr--;
-// And swap the last element with it
-        temp = arra1[ctr];
-        arra1[ctr] = arra1[index];
-        arra1[index] = temp;
-    }
-    return arra1;
-}
+let table = [];
+let pairs = 0;
 
 for (let i = 0; i < nbCards; i++){
     let cardC = document.createElement("div");   
@@ -43,42 +29,88 @@ for (let i = 0; i < nbCards; i++){
     divRow.appendChild(cardC);
 }
 
-let randomColor = shuffle(colorTable);
+function shuffle(tab) {
+    let tl = tab.length, temp, index;
 
-let nbPlayingCards = document.getElementsByClassName("playingCard");
+// While there are elements in the array
+    while (tl > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * tl);
+// Decrease tl by 1
+        tl--;
+// And swap the last element with it
+        temp = tab[tl];
+        tab[tl] = tab[index];
+        tab[index] = temp;
+    }
+    return tab;
+}
+
+
+function resetCard (table){
+    table[0].classList.remove("colorOne", "colorTwo", "colorThree", "colorFour", "colorFive", "colorSix");
+    table[1].classList.remove("colorOne", "colorTwo", "colorThree", "colorFour", "colorFive", "colorSix");
+    
+    table[0].classList.add("baseColor");
+    table[1].classList.add("baseColor");
+}
+
+function stopGame(errorText) {
+    main.style.pointerEvents="none";
+    let stop = confirm(errorText);
+    if(stop){
+        main.style.pointerEvents="auto";
+    } else {
+        stopGame();
+    }
+}
 
 function checkCards (compareTable, table) {
+    let pairOfCards = 0;
     if (compareTable.length === 2 ){
-        if (compareTable[0] !== compareTable[1]){
-            table[0].classList.remove("colorOne", "colorTwo", "colorThree", "colorFour", "colorFive", "colorSix");
-            table[1].classList.remove("colorOne", "colorTwo", "colorThree", "colorFour", "colorFive", "colorSix");
-            
-            table[0].classList.add("baseColor");
-            table[1].classList.add("baseColor");
+        if(table[0] === table[1]){
+            stopGame("ne cliquez pas deux foissur le même carte svp");
+            resetCard(table);
+        }else{
+            if (compareTable[0] !== compareTable[1]){
+                resetCard(table);
+            } else {
+                pairOfCards ++;
+            }
         }
         compareTable = [];
         table = [];
    }
-   return [compareTable,table];
+   return [compareTable,table,pairOfCards];
 }
 
-console.log(randomColor);
+let nbPlayingCards = document.getElementsByClassName("playingCard");
+let randomColor = shuffle(colorTable);
 
-let table = [];
+
 
 for (let i = 0; i < nbPlayingCards.length; i++){
     nbPlayingCards[i].addEventListener("click", function() {
         this.classList.add(randomColor[i]);
         compareTable.push(randomColor[i]);
-
         table.push(this);
+        main.style.pointerEvents = "none";
         setTimeout(function(){
-            [compareTable,table] = checkCards(compareTable,table);
-        }, 2000);
+            [compareTable,table,pairOfCards] = checkCards(compareTable,table);
+            pairs += pairOfCards;
+            console.log("Za Warudo!!!");
+            console.log(pairs);
+            main.style.pointerEvents = "auto";  
+            if (pairs === nbCards/2){
+                console.log("j'ai gagné");
+                let replay = confirm("GG, voulez-vous rejouer ?");
+                if(replay){
+                    document.location.reload(true);
+                }else {
+                    alert("aurevoir");
+                }
+            }            
+        }, 2000);     
     });
-    
+
 }
-// compareTable[0] === compareTable[1]
-
-
-// console.log(shuffle(colorTable));
